9a5673839001450594399e3087bd1532c81c583e
Amal Francis <amalfra@gmail.com>
2014-09-12 21:48:20 +0530

    Fixed bug with reported success message page.

--- Keypic/root/keypic.php
+++ Keypic/root/keypic.php
@@ -11,51 +11,61 @@ $auth->acl($user->data);
 $user->setup();
 
 $action = request_var('action', '');
-
 include_once($phpbb_root_path . 'includes/Keypic.'. $phpEx);
 Keypic::setFormID($config['keypic_Formid']);
 Keypic::setUserAgent("User-Agent: phpBB/".$config['version']." | Keypic/".$config['keypic_version']);
 
-switch($action){
-	case 'report': 
+
+switch($action)
+{
+	case 'report':
 		if (($t = request_var('t', '')) != '')
 		{
-			$topics = 'SELECT topic_first_post_id FROM ' . TOPICS_TABLE . '
-				WHERE topic_id = ' . $t ;
-				
-			$topics_result = $db->sql_query_limit($topics, 1);
-
-			while( $topics_row = $db->sql_fetchrow($topics_result) )
-			{
-				$posts = 'SELECT KeypicToken FROM ' . POSTS_TABLE . '
-				WHERE post_id = ' . $topics_row['topic_first_post_id'] ;
-				
-				$posts_result = $db->sql_query_limit($posts, 1);
-
-				while( $posts_row = $db->sql_fetchrow($posts_result) )
-				{
-					Keypic::reportSpam($posts_row['KeypicToken']);
-					trigger_error(sprintf($user->lang['KP_SPAM_REPORTED_MSG'], $user->lang['KP_TOPIC']));
-				}
-			}
+			$sql     = 'SELECT topic_first_post_id FROM ' . TOPICS_TABLE . ' WHERE ' .  $db->sql_in_set('topic_id', $t);
+			$result     = $db->sql_query($sql);
+			$row = $db->sql_fetchrow($result);
+			
+			$sql     = 'SELECT * FROM ' . POSTS_TABLE . ' WHERE ' .  $db->sql_in_set('post_id', $row['topic_first_post_id']);
+			$result     = $db->sql_query($sql);
+			$row = $db->sql_fetchrow($result);
+	
+			if (Keypic::reportSpam($row["KeypicToken"]) != 'error')
+				trigger_error(sprintf($user->lang['KP_SPAM_REPORTED_MSG'], $user->lang['KP_TOPIC']));
+			else
+				redirect(append_sid("{$phpbb_root_path}index.$phpEx"));
 		}
 		else if (($p = request_var('p', '')) != '')
 		{
-			$posts = 'SELECT KeypicToken FROM ' . POSTS_TABLE . '
-            WHERE post_id = ' . $p ;
+			$sql     = 'SELECT * FROM ' . POSTS_TABLE . ' WHERE ' .  $db->sql_in_set('post_id', $p);
+			$result     = $db->sql_query($sql);
+			$row = $db->sql_fetchrow($result);
 			
-			$posts_result = $db->sql_query_limit($posts, 1);
-
-			while( $posts_row = $db->sql_fetchrow($posts_result) )
-			{
-				Keypic::reportSpam($posts_row['KeypicToken']);
+			if (Keypic::reportSpam($row["KeypicToken"]) != 'error')
 				trigger_error(sprintf($user->lang['KP_SPAM_REPORTED_MSG'], $user->lang['KP_POST']));
-			}			
+			else
+				redirect(append_sid("{$phpbb_root_path}index.$phpEx"));
 		}
-		
-		trigger_error('Post/Topic not found');
 	break;
 	
-	default: echo 'Invalid Action';
+	case 'reportu':
+		if (($u = request_var('u', '')) != '')
+		{
+			$users = 'SELECT KeypicToken FROM ' . USERS_TABLE . '
+            WHERE user_id = ' . $u ;
+			
+			$users_result = $db->sql_query_limit($users, 1);
+			$users_row = $db->sql_fetchrow($users_result);
+			
+			if (Keypic::reportSpam($users_row['KeypicToken']) != 'error')
+				trigger_error(sprintf($user->lang['KP_SPAM_REPORTED_MSG'], $user->lang['KP_USER']));
+			else
+				redirect(append_sid("{$phpbb_root_path}index.$phpEx"));
+		}
+		
+		redirect(append_sid("{$phpbb_root_path}index.$phpEx"));
+	break;
+	
+	default:
+		echo 'Invalied Action';
 	break;
 }
